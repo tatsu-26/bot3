3ac8ea425cc07c82479351cae765fe64b508e43b
tatsu-26 <0gc2p.1g1m4471z@ezweb.ne.jp>
2019-09-29 13:16:29 +0900

    a

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,18 +1,37 @@
 class LinebotController < ApplicationController
   require "line/bot"
+  require "wikipedia"
+
   protect_from_forgery :except => [:callback]
+
+  def client
+    @client ||= Line::Bot::Client.new { |config|
+      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]  
+    }
+  end
 
   def callback
     body = request.body.read
 
-    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    signature = request.env["HTTP_X_LINE_SIGNATURE"]
     unless client.validate_signature(body, signature)
-      head :bad_request
+      error 400 do "Bad Request" end
     end
 
     events = client.parse_events_from(body)
 
     events.each { |event|
+      if event.message["text"] != nil
+        word = event.message["text"]
+        Wikipedia.Configure {
+          domain "ja.wikipedia.org"
+          path "w/api.php"
+        }
+      end
+
+      page = Wikipedia.find(word)
+      response = page.summary + "\n" + page.fullurl
       case event
       when Line::Bot::Event::Message
         case event.type
@@ -25,6 +44,7 @@ class LinebotController < ApplicationController
         end
       end
     }
+
     head :ok
   end
 end
