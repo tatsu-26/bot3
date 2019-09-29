c4b90a4875198ee1bdbcfdb73a5901cbd89f9f1c
tatsu-26 <0gc2p.1g1m4471z@ezweb.ne.jp>
2019-09-29 13:47:43 +0900

    a

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,46 +1,55 @@
 class LinebotController < ApplicationController
-  require "line/bot"
-  require "wikipedia"
+  require 'line/bot'
+  require 'wikipedia'
 
+  # callbackアクションのCSRFトークン認証を無効
   protect_from_forgery :except => [:callback]
 
   def client
     @client ||= Line::Bot::Client.new { |config|
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]  
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
     }
   end
 
   def callback
     body = request.body.read
 
-    signature = request.env["HTTP_X_LINE_SIGNATURE"]
+    signature = request.env['HTTP_X_LINE_SIGNATURE']
     unless client.validate_signature(body, signature)
-      error 400 do "Bad Request" end
+      head :bad_request
     end
 
     events = client.parse_events_from(body)
 
     events.each { |event|
-      if event.message["text"] != nil
-        word = event.message["text"]
+      if event.message['text'] != nil
+        # LINEで送られてきた文書を取得
+        word = event.message['text']
+        # 日本語版Wikipediaを設定
         Wikipedia.Configure {
-          domain "ja.wikipedia.org"
-          path "w/api.php"
+          domain 'ja.wikipedia.org'
+          path   'w/api.php'
         }
       end
 
+      # wikipediaから情報取得
       page = Wikipedia.find(word)
+
+      # 概要とURLを返す
       response = page.summary + "\n" + page.fullurl
+
       case event
+      # メッセージが送信された場合
       when Line::Bot::Event::Message
         case event.type
+        # メッセージが送られて来た場合
         when Line::Bot::Event::MessageType::Text
           message = {
-            type: "text",
-            text: event.message["text"]
+            type: 'text',
+            text: response
           }
-          client.reply_message(event["replyToken"], message)
+          client.reply_message(event['replyToken'], message)
         end
       end
     }
