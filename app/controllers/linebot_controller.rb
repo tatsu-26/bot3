a86efbe3dd33c7c46e3885f4724dda9d50383c02
tatsu-26 <0gc2p.1g1m4471z@ezweb.ne.jp>
2019-10-01 16:02:58 +0900

    a

--- app/controllers/linebot_controller.rb
+++ app/controllers/linebot_controller.rb
@@ -1,48 +1,50 @@
 class LinebotController < ApplicationController
-  require 'line/bot'  # gem 'line-bot-api'
-  require 'open-uri'
-  # callbackアクションのCSRFトークン認証を無効
+  require "line/bot"
+  require "wikipedia"
+
   protect_from_forgery :except => [:callback]
 
   def client
     @client ||= Line::Bot::Client.new { |config|
       config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]  
     }
   end
 
   def callback
     body = request.body.read
 
-    signature = request.env['HTTP_X_LINE_SIGNATURE']
+    signature = request.env["HTTP_X_LINE_SIGNATURE"]
     unless client.validate_signature(body, signature)
-      head :bad_request
+      error 400 do "Bad Request" end
     end
 
     events = client.parse_events_from(body)
 
     events.each { |event|
+      if event.message["text"] != nil
+        word = event.message["text"]
+        Wikipedia.Configure {
+          domain "ja.wikipedia.org"
+          path "w/api.php"
+        }
+      end
+
+      page = Wikipedia.find(word)
+      response = page.summary + "\n" + page.fullurl + page.images
       case event
       when Line::Bot::Event::Message
         case event.type
         when Line::Bot::Event::MessageType::Text
           message = {
-            type: 'text',
-            text: return_message
+            type: "text",
+            text: event.message["text"]
           }
-          client.reply_message(event['replyToken'], message)
+          client.reply_message(event["replyToken"], message)
         end
       end
     }
 
     head :ok
-  end
-  def return_message
-    open("http://www.meigensyu.com/quotations/view/random") do |file|
-      page = file.read
-      page.scan(/<div class=\"text\">(.*?)<\/div>/).each do |meigen|
-        return meigen[0].encode("sjis")
-      end
-    end
   end
 end
